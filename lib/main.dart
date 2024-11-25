6f85f61e4ef6321813bebcaf991aafc9d2e679be
ykcnngl <120281995+ykcnngl@users.noreply.github.com>
2024-11-25 16:05:02 +0300

    bağlantı çalışmıyor

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              ' bağlantı çalışmıyor',
             ),
             Text(
               '$_counter',
