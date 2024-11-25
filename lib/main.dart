fe8ea23fd44a4afc0fc5d41ed13be2fabd81e285
Badeb <badebalci2002@gmail.com>
2024-11-25 18:08:19 +0300

    appbar and navigationbar created.

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,15 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'ECODRIVE'),
-
+      home: const MyHomePage(
+        title: 'ECODRIVE',
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -33,48 +33,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Colors.orange,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        leading: Icon(Icons.menu),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.person),
+              onPressed: () {
+
+              }
+          )
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              ' badenin bilgisayarı çalışıyor',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.orange, // Arka plan rengi
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.directions_car),
+              label: " ",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: " ",
+            )
+          ]),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
