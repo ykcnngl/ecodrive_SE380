021de6a508735fce4998b7c207471441870e145f
Yekta Kağan Cananoğlu <120281995+ykcnngl@users.noreply.github.com>
2024-12-23 00:50:02 +0300

    Update signup.dart
    pixel issues fixed

--- lib/Opening/signup.dart
+++ lib/Opening/signup.dart
@@ -21,35 +21,82 @@ class Signup extends StatelessWidget {
     final password = passwordController.text.trim();
     final birthday = birthdayController.text.trim();
 
-    if (ekoid.isEmpty || password.isEmpty || birthday.isEmpty || name.isEmpty || surname.isEmpty ) {
+    if (ekoid.isEmpty ||
+        password.isEmpty ||
+        birthday.isEmpty ||
+        name.isEmpty ||
+        surname.isEmpty) {
       showDialog(
         context: context,
-        builder: (context) =>
-            AlertDialog(
-              title: const Text("ERROR"),
-              content: const Text("Please fill  all the spaces"),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Okay"),
-                ),
-              ],
+        builder: (context) => AlertDialog(
+          title: const Text("ERROR"),
+          content: const Text("Please fill  all the spaces"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Okay"),
             ),
+          ],
+        ),
       );
       return;
     }
     final DateTime birthDate = DateTime.parse(birthday);
     final DateTime today = DateTime.now();
-    final int age = today.year-birthDate.year-((today.month < birthDate.month|| (today.month == birthDate.month && today.day<birthDate.day))? 1:0);
-    if(age<18){
+    final int age = today.year -
+        birthDate.year -
+        ((today.month < birthDate.month ||
+                (today.month == birthDate.month && today.day < birthDate.day))
+            ? 1
+            : 0);
+    if (age < 18) {
       showDialog(
         context: context,
-        builder: (context) =>
-            AlertDialog(
+        builder: (context) => AlertDialog(
+          title: const Text("ERROR"),
+          content: const Text("You must be at least 18 years old "),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Okay"),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
+    try {
+      final userDoc =
+          await FirebaseFirestore.instance.collection('users').doc(ekoid).get();
+      if (userDoc.exists) {
+        if (userDoc['name'] == name &&
+            userDoc['surname'] == surname &&
+            userDoc['birthday'] == birthday) {
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(ekoid)
+              .update({
+            'password': password,
+          });
+
+          globals.currentEkoId = ekoid;
+
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const MyHomePage()),
+          );
+        } else {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
               title: const Text("ERROR"),
-              content: const Text("You must be at least 18 years old "),
+              content: const Text(
+                  "Please fill in your information in the same way as on your school card. "),
               actions: [
                 TextButton(
                   onPressed: () {
@@ -59,67 +106,27 @@ class Signup extends StatelessWidget {
                 ),
               ],
             ),
-      );
-      return;
-
-    }
-
-
-
-    try {
-      final userDoc = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(ekoid)
-          .get();
-      if (userDoc.exists ){
-        if(userDoc['name'] == name && userDoc['surname'] == surname && userDoc['birthday']==birthday) {
-          await FirebaseFirestore.instance.collection('users').doc(ekoid).update({'password':password,
-          });
-
-          globals.currentEkoId = ekoid;
-
-          Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const MyHomePage()),
-          );
-        } else {
-          showDialog(
-            context: context,
-            builder: (context) =>
-                AlertDialog(
-                  title: const Text("ERROR"),
-                  content: const Text("Please fill in your information in the same way as on your school card. "),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text("Okay"),
-                    ),
-                  ],
-                ),
           );
         }
       }
     } catch (e) {
       showDialog(
         context: context,
-        builder: (context) =>
-            AlertDialog(
-              title: const Text("ERROR"),
-              content: const Text("Incorrect EkoID or password."),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Okey"),
-                ),
-              ],
+        builder: (context) => AlertDialog(
+          title: const Text("ERROR"),
+          content: const Text("Incorrect EkoID or password."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Okey"),
             ),
+          ],
+        ),
       );
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -134,77 +141,82 @@ class Signup extends StatelessWidget {
             },
           ),
         ),
-        Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 200),
-              Text("Welcome  ",
-                  style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepOrange[700])),
-              //ekoid
-              const SizedBox(height: 25),
-              myTextfield(
-                controller: EkoIdController,
-                hintText: "EkoID",
-                obscureText: false,
-              ),
-              //name
-              const SizedBox(height: 10),
-              myTextfield(
-                controller: nameController,
-                hintText: "Name",
-                obscureText: false,
-              ),
-              //surname
-              const SizedBox(height: 10),
-              myTextfield(
-                controller: surnameController,
-                hintText: "Surname",
-                obscureText: false,
-              ),
+        SingleChildScrollView(                      // we added scroll due to same issue from login page
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 200),
+                Text("Welcome  ",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange[700])),
+                //ekoid
+                const SizedBox(height: 25),
+                myTextfield(
+                  controller: EkoIdController,
+                  hintText: "EkoID",
+                  obscureText: false,
+                ),
+                //name
+                const SizedBox(height: 10),
+                myTextfield(
+                  controller: nameController,
+                  hintText: "Name",
+                  obscureText: false,
+                ),
+                //surname
+                const SizedBox(height: 10),
+                myTextfield(
+                  controller: surnameController,
+                  hintText: "Surname",
+                  obscureText: false,
+                ),
 
-              //pasword
-              const SizedBox(height: 10),
-              myTextfield(
-                controller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-              ),
+                //pasword
+                const SizedBox(height: 10),
+                myTextfield(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
 
-              const SizedBox(height: 10),
-              GestureDetector(
-                onTap: () async {
-                  DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime(2006),
-                    firstDate: DateTime(1960),
-                    lastDate: DateTime.now(),
-                  );
-                  if (selectedDate != null) {
-                    birthdayController.text =
-                    "${selectedDate.toLocal()}".split(' ')[0];
-                  }
-                },
-                child: AbsorbPointer(
-                    child: myTextfield(
-                      controller: birthdayController,
-                      hintText: "Birthday",
-                      obscureText: false,
-                    )),
-              ),
-              //Login
-              const SizedBox(height: 25),
-              Myloginbutton(
-                onTap: () {
-                  SignupUser(context);
-                },
-              ),
-            ],
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () async {
+                    DateTime? selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime(2006),
+                      firstDate: DateTime(1960),
+                      lastDate: DateTime.now(),
+                    );
+                    if (selectedDate != null) {
+                      birthdayController.text =
+                          "${selectedDate.toLocal()}".split(' ')[0];
+                    }
+                  },
+                  child: AbsorbPointer(
+                      child: myTextfield(
+                    controller: birthdayController,
+                    hintText: "Birthday",
+                    obscureText: false,
+                  )),
+                ),
+                //Login
+                const SizedBox(height: 25),
+                Myloginbutton(
+                  onTap: () {
+                    SignupUser(context);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ]),
     );
   }
 }
+
+
