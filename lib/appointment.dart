a75778f6b59aa39d0575c114e45048dbe774bf10
Yekta Kağan Cananoğlu <120281995+ykcnngl@users.noreply.github.com>
2024-12-29 02:13:27 +0300

    Update appointment.dart
    There is some errors

--- lib/appointment.dart
+++ lib/appointment.dart
@@ -1,16 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Appointment extends StatelessWidget {
-  const Appointment({
-    Key? key,
-  }) : super(key: key);
+  const Appointment({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Your Appointments"),
-
+        title: const Text("Your Appointments"),
+        backgroundColor: Colors.orange,
       ),
+      body: StreamBuilder(stream: FirebaseFirestore.instance.collection('appointments').doc("driverEkoId").snapshots(),
+          builder: (context,snapshot) {
+        if(!snapshot.hasData){
+          return const Center(
+          child: CircularProgressIndicator()
+          );
+    }
+        final appointmentData = snapshot.data!.data() as Map<String, dynamic>;
+        final passengers = appointmentData['passengers'] as List<dynamic>;
+
+        return ListView.builder(
+    itemCount: passengers.length,
+          itemBuilder: (context, index) {
+            final passengerId = passengers[index];
+            return ListTile(
+    title: Text("Passenger $passengerId"),
     );
+    }
+
+    );
+
+
+    },
+    ));
   }
 }
+
+//
